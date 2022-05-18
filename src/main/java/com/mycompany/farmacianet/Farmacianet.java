e95769eed950ea6bfb33040f377d58e4b648249e
Yahaira Tituaña <yatg2495@gmail.com>
2022-05-18 18:26:29 -0500

    El primer commit :D

---
+++ src/main/java/com/mycompany/farmacianet/Farmacianet.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.farmacianet;
+
+/**
+ *
+ * @author USER
+ */
+public class Farmacianet {
+
+    public static void main(String[] args) {
+        Gestion vtn = new Gestion();
+        vtn.setVisible(true);
+    }
+}
